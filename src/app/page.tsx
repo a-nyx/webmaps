f7a49ca7baaa184c82e2234f32601f91f4eaea04
a-nyx <NyxAdam@protonmail.com>
2023-12-03 19:07:15 -0500

    squash basemap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Basemap from '@/components/Basemap';
+
 const Home = () => {
-  return <main>ahahah</main>;
+  return (
+    <main>
+      <h1>Web Maps: Open source & Serverless</h1>
+      <Basemap />
+    </main>
+  );
 };
 
 export default Home;
